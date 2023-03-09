25ca3e6a76ad50b24dfa4aaadc5648e4fba0cf3b
Asma <asmabaker94@gmail.com>
2023-03-08 21:52:24 -0500

    update

--- src/test/java/com/fidexio/step_definitions/Eventmanager_steps.java
+++ src/test/java/com/fidexio/step_definitions/Eventmanager_steps.java
@@ -1,34 +1,47 @@
 package com.fidexio.step_definitions;
 
-import com.fidexio.pages.B28G25_67_PAGE;
 import com.fidexio.pages.LoginPage;
-import com.fidexio.utilities.ConfigurationReader;
+import com.fidexio.pages.US_67page;
+import com.fidexio.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 
-public class Eventmanager_steps{
-
-    B28G25_67_PAGE b28G25_67_page= new B28G25_67_PAGE();
+public class Eventmanager_steps {
     LoginPage page = new LoginPage();
-    @When("user enters valid credentials and clicks on login button")
-    public void userEntersValidCredentialsAndClicksOnLoginButton() {
-        page.login(ConfigurationReader.getProperty("events_manager_username"),ConfigurationReader.getProperty("events_manager_password"));
+    US_67page pagemodul = new US_67page();
+    List<WebElement> Allmodules =pagemodul.listofmodules;
 
-        page.submit.click();
-    }
-    @Then("user should be able to see all modules")
-    public void user_should_be_able_to_see_all_modules(){
+    @Then("user should see all {int} modules")
+    public void userShouldSeeAllModules(int expected){
 
+        for(WebElement eachModule :Allmodules) {
+            Assert.assertEquals(expected, Allmodules.size());
 
-       b28G25_67_page.allmodules.isEnabled();
-    }
-    @Then("user should access different modules")
-    public void user_should_access_different_modules(){
-
-        b28G25_67_page.allmodules.isEnabled();
+        }
 
 
     }
 
+    @Then("user is able to access all modules")
+    public void userIsAbleToAccessAllModules() {
+
+        for (WebElement eachModule : Allmodules) {
+            Assert.assertTrue(eachModule.isEnabled());
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
--- src/test/resources/features/Eventmanager.feature
+++ src/test/resources/features/Eventmanager.feature
@@ -2,10 +2,11 @@ Feature: Event Manager accessibility
 
   Agile story: As an Event manager I should be able to access different modules
 
-  @US67.Asma
-
-  Scenario: Event manager should access all modules
+  Background: User is already in the log in page
     Given the user is on the login page
-    When user enters valid credentials and clicks on login button
-    Then user should be able to see all modules
-    Then user should access different modules
+
+@Asma
+  Scenario: Event manager should access all modules
+    When the user logged in with username as "eventscrmmanager10@info.com" and password as "eventscrmmanager"
+    Then user should see all 15 modules
+    Then user is able to access all modules
